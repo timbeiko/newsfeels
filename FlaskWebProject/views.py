b9a27a3dd65db564ad7742fc006592da62634ceb
tbeiko <t.beiko23@gmail.com>
2015-11-14 20:22:31 -0500

    try something

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -11,17 +11,17 @@ from html_ripper import main
 from flask import request
 from flask import abort, redirect, url_fo
 
-# if __name__ == '__main__':
-#     app.debut = True
-#     app.run()
+if __name__ == '__main__':
+    app.debut = True
+    app.run()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        article = main('hi')
-        return redirect(url_for('music', article = "hi"))
-    else:
-        return render_template('index.html')
+    # if request.method == 'POST':
+    #     # article = main('hi')
+    #     return redirect(url_for('music', article = "hi"))
+    # else:
+    return render_template('index.html')
 
 @app.route('/contact')
 def contact():
