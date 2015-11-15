a59a830ab210fdc065ccde4698af98b677aed03f
tbeiko <t.beiko23@gmail.com>
2015-11-15 09:48:54 -0500

    define app

--- views.py
+++ views.py
@@ -10,6 +10,9 @@ from flask import request
 from flask import abort, redirect, url_for
 from html_rip import link_or_nah, to_text, get_html, visible
 from mood import determineSubject
+from flask import Flask
+
+app = Flask(__name__)
 
 if __name__ == '__main__':
     app.debut = True
